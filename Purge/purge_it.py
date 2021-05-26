f11bd68f487136674ac7028ccda73c0e72ad6938
sumedhdk <sumedhdkme19>
2021-05-26 18:02:29 +0530

    Linux Commit

--- Purge/purge_it.py
+++ Purge/purge_it.py
@@ -10,7 +10,7 @@ SCOPES = ["https://www.googleapis.com/auth/drive.activity.readonly https://www.g
 
 def action(user_id, course_id, lapse):
     student = {
-        'userId': 'me'
+        'userId': 1234
     }
     try:
         student = service.courses().students().delete(
@@ -44,18 +44,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('../data_collectors/token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('../data_collector/token.json'):
+        creds = Credentials.from_authorized_user_file('../data_collector/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '../data_collector/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('../data_collector/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('classroom', 'v1', credentials=creds)
@@ -74,4 +74,3 @@ def main():
 if __name__ == '__main__':
     main()
     purging()
-
